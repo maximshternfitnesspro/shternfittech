e6448a016c39b85b6267865ffc7458f3be5a6a49
maximshternfitnesspro <mvetrov284@gmail.com>
2026-02-19 03:50:39 +0300

    Summary

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from telegram import (
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, MessageHandler, filters
 
 
-ASSET_VERSION = "20260218g"
+ASSET_VERSION = "20260218h"
 
 TRIBUTE_LINKS = {
     "CORE": {
